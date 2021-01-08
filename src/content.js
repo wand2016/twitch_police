129fddd18c277d5d27e8b6535225278e9bd8d497
wand <d19921207@gmail.com>
2021-01-08 22:39:03 +0900

    fixed

--- src/content.js
+++ src/content.js
@@ -1,6 +1,14 @@
+console.log('twitch-police loaded');
+
 window.addEventListener('load', () => {
+  var working = false;
+  console.log('twitch-police ready');
   document.querySelector('textarea[data-test-selector="chat-input"]')
     .addEventListener('input', (e) => {
+      if (!working) {
+        working = true;
+        console.log('twitch-police working');
+      }
       e.target.value = e.target.value
         .replaceAll(/レギュレーション/g,'カテゴリ')
         .replaceAll(/レギュ/g,'カテゴリ')
